3ac7e38e1b7b9898ddb44e36fbfd25c36ee6545e
LQSYMichaelLuo <99405224+LQSYMichaelLuo@users.noreply.github.com>
2023-02-18 17:02:40 +0800

    Create getPathsOfAFolder.lua

---
+++ FileUtil/getPathsOfAFolder.lua
@@ -0,0 +1,57 @@
+--Warning: Please use this code under the framework of Androlua+ !!!
+--MIT License
+--Author: MichaelLuo
+
+--导入所需库
+require("import")
+import("java.io.File")
+
+--获取某目录路径下所有文件的绝对路径
+--@parameter:
+--basicPath string 
+--@return:
+--absolutePathsTable table 
+function getTheAbsolutePathsTableOfAllFilesUnderThePath(basicPath)
+  local absolutePathsTable = {}
+  local fileBasicPath = File(basicPath)
+  local tab = fileBasicPath.listFiles()
+  for i = 0, #tab - 1 do
+    local userdata = tab[i]
+    if userdata.isDirectory() then
+      local absolutePathsTable_child = getTheAbsolutePathsTableOfAllFilesUnderThePath(tostring(userdata))
+      for i = 1, #absolutePathsTable_child do
+        absolutePathsTable[#absolutePathsTable+1] = absolutePathsTable_child[i]
+      end
+    else
+      absolutePathsTable[#absolutePathsTable+1] = tostring(userdata)
+    end
+    luajava.clear(userdata)
+  end
+  luajava.clear(fileBasicPath)
+  return absolutePathsTable
+end
+
+--获取某目录路径下所有文件的相对路径
+--@parameter:
+--path string 
+--@return:
+--relativePathsTable table 
+function getTheRelativePathsTableOfAllFilesUnderThePath(path)
+  local theLastCharacterOfPathString = path:sub(#path,#path)
+  if theLastCharacterOfPathString ~= "/" then
+    path = path .. "/"
+  end
+  local relativePathsTable = {}
+  local absolutePathsTable = getTheAbsolutePathsTableOfAllFilesUnderThePath(path)
+  for i = 1, #absolutePathsTable do
+    relativePathsTable[#relativePathsTable+1] = absolutePathsTable[i]:match(path.."(.+)")
+  end
+  return relativePathsTable
+end
+
+--使用示例：
+local path = "/storage/emulated/0/Androlua/"
+local relativePaths = getTheRelativePathsTableOfAllFilesUnderThePath(path)
+local absolutePaths = getTheAbsolutePathsTableOfAllFilesUnderThePath(path)
+print("以下是" .. path .. "下所有文件的相对路径列表" .. dump(relativePaths))
+print("以下是" .. path .. "下所有文件的绝对路径列表" .. dump(absolutePaths))
